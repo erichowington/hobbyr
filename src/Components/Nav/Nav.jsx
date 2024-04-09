b333bb69103abfd8ae651032bcd081ba543e6fe3
Shatlykch <157189400+Shatlykch@users.noreply.github.com>
2024-04-09 10:26:47 -0400

    routes for screens and navlinks rendered

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react';
+
 import "./Nav.css"
 
-function Nav() {
+const navOptions = (
+    
+  <>
+    <NavLink className='nav-link' to='/profile'>User Profile</NavLink>
+    <NavLink className='nav-link' to='/feed'>Feed</NavLink>
+    <NavLink className='nav-link' to='/createproject'>Create Project</NavLink>
+    <NavLink className='nav-link' to='/browse'>Browse</NavLink>
+  </>)
+
+function Nav () {
+
+ 
+
+
   return (
-    <div>Nav</div>
+    <div className='nav-container'>
+      <div className='links'> { navOptions} </div>
+    </div>
   )
 }
 
