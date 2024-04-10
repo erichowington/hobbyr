19be12e4ec78ffed3534eda874862f550cd45633
erichowington <ericehowington@gmail.com>
2024-04-10 13:35:03 -0400

    sign up page

--- src/Screens/SignIn/SignIn.jsx
+++ src/Screens/SignIn/SignIn.jsx
@@ -1,107 +1,114 @@
-import React from 'react'
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { signIn } from '../../Services/users.js';
-import "./SignIn.css"
-
-
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signIn } from "../../Services/users.js";
+import "./SignIn.css";
 
 function SignIn({ setUser }) {
   const navigate = useNavigate();
 
-  const [form, setForm] = useState( {
-    username: '',
-    password: '',
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
     isError: false,
-    errorMsg: '',
-  } );
+    errorMsg: "",
+  });
 
-  const handleChange = ( e ) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
 
-    setForm( ( prevForm ) => ( {
+    setForm((prevForm) => ({
       ...prevForm,
       [name]: value,
-    } )
-    )
+    }));
   };
 
-const handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-        const userData = await signIn(form);
-        setUser(userData);
+      const userData = await signIn(form);
+      setUser(userData);
 
-        navigate('/browse');
+      navigate("/browse");
     } catch (error) {
-        console.error(error);
-        setForm((prevForm) => ({
-            ...prevForm,
-            isError: true,
-            errorMsg: "Invalid Credentials",
-            password: '',
-        }));
+      console.error(error);
+      setForm((prevForm) => ({
+        ...prevForm,
+        isError: true,
+        errorMsg: "Invalid Credentials",
+        password: "",
+      }));
     }
+  };
+
+  const renderError = () => {
+    const toggleForm = form.isError ? "danger" : "";
+
+    if (form.isError) {
+      return (
+        <button type="submit" className={toggleForm}>
+          {" "}
+          {form.errorMsg}
+        </button>
+      );
+    } else {
+      return (
+        <button className="signin-button" type="submit">
+          Log In
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div className="signin-wrapper">
+      <div className="siginin-logo-wrapper">
+        <img
+          className="signin-logo-img"
+          src="https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-logos/hobbyr-logo-orange.png?raw=true"
+        />
+      </div>
+      <div className="signin-container">
+        <form className="signin-form" onSubmit={handleSubmit}>
+          <div className="signin-user-wrapper">
+            <input
+              className="signin-user"
+              type="text"
+              name="username"
+              value={form.username}
+              placeholder="Enter Username"
+              onChange={handleChange}
+              required
+              autoComplete="off"
+            />
+          </div>
+
+          <div className="signin-password-wrapper">
+            <input
+              className="signin-password"
+              type="password"
+              name="password"
+              value={form.password}
+              placeholder="Enter Password"
+              onChange={handleChange}
+              required
+              autoComplete="off"
+            />
+          </div>
+
+          {renderError()}
+        </form>
+        <Link
+        className="link-sign-in"
+        to={"/signup"}
+      >
+        No account?
+        Sign up <span className="here">here.</span>
+      </Link>
+      </div>
+    </div>
+  );
 }
 
-const renderError = () => {
-  const toggleForm = form.isError ? "danger" : '';
-
-  if( form.isError ) {
-    return ( <button type='submit' className={toggleForm}> {form.errorMsg}</button>
-    );
-  } else {
-    return <button className='signin-button' type='submit'>Log In</button>
-  }
-  }
-
-
-
-
-    return (
-        <div className='signin-wrapper'>
-        <div className='siginin-logo-wrapper'>
-          <img className='signin-logo-img'src='https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-logos/hobbyr-logo-orange.png?raw=true'/>
-        </div>
-            <div className='signin-container'>
-        
-                <form className='signin-form' onSubmit={handleSubmit}>
-
-                  <div className='signin-user-wrapper'>
-                    <input className='signin-user' type="text"
-                        name='username'
-                        value={form.username}
-                        placeholder='Enter Username'
-                        onChange={handleChange}
-                        required
-                        autoComplete='off'
-                    />
-                  </div>
-                    
-                  
-                  <div className='signin-password-wrapper'>
-                    <input className='signin-password' type="password"
-                        name='password'
-                        value={form.password}
-                        placeholder='Enter Password'
-                        onChange={handleChange}
-                        required
-                        autoComplete='off'
-                    />
-                  </div>
-
-                    {renderError()}
-                    
-
-                </form>
-            </div>
-
-
-        </div>
-    );
-}
-
-
-
-export default SignIn
+export default SignIn;
--- src/Screens/SignUp/SignUp.jsx
+++ src/Screens/SignUp/SignUp.jsx
@@ -1,110 +1,116 @@
-import React from 'react'
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { signUp } from '../../Services/users.js';
-
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signUp } from "../../Services/users.js";
+import "./SignUp.css"
 
 function Register({ setUser }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [form, setForm] = useState({
-        username: '',
-        email: '',
-        password: '',
-        isError: false,
-        errorMsg: '',
-    });
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    isError: false,
+    errorMsg: "",
+  });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-        setForm(prevForm => ({
-            ...prevForm,
-            [name]: value,
-        }));
-    };
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        try {
-            const userData = await signUp(form);
-            console.log(userData)
-            setUser(userData);
+    try {
+      const userData = await signUp(form);
+      console.log(userData);
+      setUser(userData);
 
-            navigate('/browse');
+      navigate("/browse");
+    } catch (error) {
+      console.error(error);
+      setForm((prevForm) => ({
+        ...prevForm,
+        isError: true,
+        errorMsg: "Invalid credentials",
+        email: "",
+        password: "",
+      }));
+    }
+  };
 
-        } catch (error) {
-            console.error(error);
-            setForm(prevForm => ({
-                ...prevForm,
-                isError: true,
-                errorMsg: 'Invalid credentials',
-                email: '',
-                password: '',
-            }));
-        }
-    };
-
-
-    const renderError = () => {
+  const renderError = () => {
     const toggleForm = form.isError ? "danger" : "";
 
     if (form.isError) {
-        return (
-            <button type='submit' className={toggleForm}>{form.errorMsg}</button>
-        );
+      return (
+        <button type="submit" className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      );
     } else {
-        return <button type="submit">Register</button>;
+      return <button className="signup-button" type="submit">Register</button>;
     }
-};
+  };
 
+  return (
+    <div className="signup-wrapper">
+      <div className="siginin-logo-wrapper">
+        <img
+          className="signin-logo-img"
+          src="https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-logos/hobbyr-logo-orange.png?raw=true"
+        />
+      </div>
+      <div className="signup-container">
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <div className="signup-user-wrapper">
+            <input
+              className="signup-user"
+              type="text"
+              name="username"
+              value={form.username}
+              placeholder="Enter Username"
+              onChange={handleChange}
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="signup-email-wrapper">
+            <input
+              className="signup-email"
+              type="text"
+              name="email"
+              value={form.email}
+              placeholder="Enter Email"
+              onChange={handleChange}
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="signup-password-wrapper">
+            <input
+              className="signup-password"
+              type="password"
+              name="password"
+              value={form.password}
+              placeholder="Enter Password"
+              onChange={handleChange}
+              required
+              autoComplete="off"
+            />
+          </div>
 
-
-
-    return (
-        <div className='signup-wrapper'>
-            <div className='signup-container'>
-        
-                <form className='signup-form' onSubmit={handleSubmit}>
-
-                    <input type="text"
-                        name='username'
-                        value={form.username}
-                        placeholder='Enter Username'
-                        onChange={handleChange}
-                        required
-                        autoComplete='off'
-                    />
-                    <input type="text"
-                        name='email'
-                        value={form.email}
-                        placeholder='Enter Email'
-                        onChange={handleChange}
-                        required
-                        autoComplete='off'
-                    />
-                    
-                    <input type="password"
-                        name='password'
-                        value={form.password}
-                        placeholder='Enter Password'
-                        onChange={handleChange}
-                        required
-                        autoComplete='off'
-                    />
-
-                    {renderError()}
-                    
-
-                </form>
-            </div>
-
-
-        </div>
-    );
+          {renderError()}
+        </form>
+      </div>
+    </div>
+  );
 }
 
-
-
-export default Register
+export default Register;
