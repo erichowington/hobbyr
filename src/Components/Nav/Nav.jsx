dac8138c102ea3e9404431f43ebe7c2b78ac3321
Booker Ngoon <bookerngoon@gmail.com>
2024-04-11 14:34:57 -0400

    user profile working, need to fix route get get own profile when button is pressed

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { useState } from 'react';
 
 import "./Nav.css"
+
+
+function Nav(){
 
 const navOptions = (
     
@@ -10,10 +12,9 @@ const navOptions = (
     <NavLink className='nav-link' to='/feed'><img className='nav-icon' src='https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-icons/home-icon.png?raw=true'/></NavLink>
     <NavLink className='nav-link' to='/createproject'><img className='nav-icon' src='https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-icons/create-icon.png?raw=true'/></NavLink>
     <NavLink className='nav-link' to='/browse'><img className='browse-icon'src='https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-icons/browse-icon.png?raw=true'/></NavLink>
-    <NavLink className='nav-link' to='/profile'><img className='nav-icon'src='https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-icons/profile-icon.png?raw=true'/></NavLink>
+    <NavLink className='nav-link' to='/profile/:profileId'><img className='nav-icon'src='https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-icons/profile-icon.png?raw=true'/></NavLink>
   </div>)
-
-function Nav () {
+ 
 
  
 
