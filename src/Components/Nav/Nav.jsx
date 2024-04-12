5f2443f9640d917a99122a54b269e7ff5ddd279d
Booker Ngoon <bookerngoon@gmail.com>
2024-04-11 22:54:35 -0400

    browse.jsx screen started

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { userProfile } from "../UserProfile/UserProfile.jsx"
 
 import "./Nav.css"
 
--- src/Screens/Browse/Browse.jsx
+++ src/Screens/Browse/Browse.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
 import "./Browse.css"
+import React, { useState, useEffect } from 'react';
+import { getProjects } from "../../Services/project";
 
-function Browse() {
+
+const Browse = () => {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    loadProjects();
+  }, []);
+
+  const loadProjects = async () => {
+    try {
+      const data = await getProjects();
+      setProjects(data);
+    } catch (error) {
+      console.error("Failed to fetch projects:", error);
+    }
+  };
+
   return (
-    <div>Browse</div>
-  )
-}
+    <div className="browse-wrapper">
+      <h1 className="browse-title">Projects</h1>
+      <div className="browse-content">
+        {projects.map((project) => (
+          <div key={project.id} className="browse-project-card">
+            <h2 className="browse-project-title">{project.project_title}</h2>
+            {project.project_img && <img src={project.project_img} alt={project.project_title} style={{ width: '10em' }} />}
+            <p>{project.body}</p>
+            {project.link && <a href={project.link} target="_blank" rel="noopener noreferrer">More Info</a>}
+            <p>Created on: {new Date(project.created_at).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Browse
+export default Browse;
+
+
+
