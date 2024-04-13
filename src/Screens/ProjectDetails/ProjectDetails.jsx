c19d7dc653a2102b0e3fd808103c06788f6f7dc3
erichowington <ericehowington@gmail.com>
2024-04-13 14:34:39 -0400

    project details css

--- src/Screens/ProjectDetails/ProjectDetails.jsx
+++ src/Screens/ProjectDetails/ProjectDetails.jsx
@@ -4,55 +4,100 @@ import { Link, useParams, useNavigate, NavLink } from "react-router-dom";
 import CommentModal from "../../Components/CommentModal/CommentModal.jsx";
 import "./ProjectDetails.css";
 
-
-
 function ProjectDetails() {
+  const PROJECT_TYPES = [
+    { code: "T", name: "Tech" },
+    { code: "C", name: "Carpentry" },
+    { code: "R", name: "Renovations" },
+    { code: "A", name: "Art & Design" },
+    { code: "J", name: "Jewelry" },
+    { code: "H", name: "Homegoods" },
+  ];
+
   const [project, setProject] = useState({});
   let { projectId } = useParams();
-  let navigate= useNavigate();
+  let navigate = useNavigate();
 
   const fetchProject = async () => {
-    const oneProject = await getProject(projectId); 
-    setProject(oneProject)
+    const oneProject = await getProject(projectId);
+    setProject(oneProject);
   };
 
   useEffect(() => {
     fetchProject();
-  }, [projectId]); 
+  }, [projectId]);
 
   const handleDelete = async (id) => {
-    await deleteProject(projectId); 
-    navigate("/feed"); 
+    await deleteProject(projectId);
+    navigate("/feed");
   };
 
-
-  
-
   return (
-    <div className="project-details">
-      <div className="detailContent">
-        <h1>{project.project_title}</h1>
-
-        {project.project_img && (
-          <img className="project-image" src={project.project_img} alt={project.project_title} />
-        )}
-        <div className="projectBody">
-          <p>{project.body}</p>
-        </div>
-        {project.link && (
-          <a href={project.link} target="_blank" rel="noopener noreferrer">Project Link</a>
-        )}
-        <div>
-          <Link to={`/projects/${projectId}/edit`}>
-            <button className="edit">EDIT</button>
-          </Link>
-          <button className="delete" onClick={handleDelete}>DELETE</button>
-        </div>
-
-        <Link to="/projects">Back to Projects</Link>
+    <div className="project-details-wrapper">
+      <div className="details-logo-wrapper">
+        <img
+          className="details-logo"
+          src="https://github.com/erichowington/hobbyr/blob/dev/public/images/hobbyr-logos/hobbyr-logo-orange.png?raw=true"
+        />
+        <Link className="details-header-link" to="/projects">back to browse</Link>
       </div>
-      <CommentModal projectId={projectId} />
-      
+      <div className="project-details">
+        <div className="detailContent">
+          <div className="project-image-wrapper">
+            <img
+              className="project-image"
+              src={project.project_img}
+              alt={project.project_title}
+            />
+          </div>
+          <div className="types-container">
+            <div className="project-title">
+              <span className="title-head">TITLE:</span>
+              {project.project_title}
+            </div>
+            <div className="project-details-type">
+              {project.project_type === "H" ? "Homegoods" : null}
+            </div>
+            <div className="project-details-type">
+              {project.project_type === "J" ? "Jewelry" : null}
+            </div>
+            <div className="project-details-type">
+              {project.project_type === "R" ? "Renovations" : null}
+            </div>
+            <div className="project-details-type">
+              {project.project_type === "C" ? "Carpentry" : null}
+            </div>
+            <div className="project-details-type">
+              {project.project_type === "T" ? "Tech" : null}
+            </div>
+            <div className="project-details-type">
+              {project.project_type === "A" ? "Art & Design" : null}
+            </div>
+          </div>
+          <div className="project-body-container">
+            <p className="project-body">{project.body}</p>
+          </div>
+          <div className="project-details-link">
+            <a
+              className="project-link"
+              href={project.link} 
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Project Link
+            </a>
+          </div>
+          <div className="my-project-options">
+            <Link to={`/projects/${projectId}/edit`}>
+              <button className="edit-the-project">EDIT</button>
+            </Link>
+            <button className="delete-the-project" onClick={handleDelete}>
+              DELETE
+            </button>
+          </div>
+        </div>
+        <CommentModal projectId={projectId} />
+      </div>
     </div>
   );
 }
