073b2a8cd3a093bf6680d6a189abf515b9572380
Booker Ngoon <bookerngoon@gmail.com>
2024-04-14 14:05:25 -0400

    follows fixed

--- src/Services/follow.js
+++ src/Services/follow.js
@@ -2,13 +2,12 @@ import api from "./apiConfig";
 
 export const getFollows = async (id) => {
   try {
-    const response = await api.get(`/followers/`);
+    const response = await api.get(`/follows/${id}/`);
     return response.data;
   } catch (error) {
     throw error;
   }
 };
-
 export const Follow = async (id) => {
   try {
     const response = await api.post(`/follow/${id}/`);
